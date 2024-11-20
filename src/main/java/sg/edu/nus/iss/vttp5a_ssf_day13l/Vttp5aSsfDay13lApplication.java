abbe1586048e364403877ba910b9350d7090218f
darryl1975 <darryl1975@hotmail.com>
2024-11-20 10:49:32 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/vttp5a_ssf_day13l/Vttp5aSsfDay13lApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5a_ssf_day13l;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5aSsfDay13lApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5aSsfDay13lApplication.class, args);
+	}
+
+}
