5cdc733b20992efc8973fc55a87b9b84936716e8
darryl1975 <darryl1975@hotmail.com>
2024-11-20 14:38:49 +0800

    CRUD ops for PErson objecT

--- src/main/java/sg/edu/nus/iss/vttp5a_ssf_day13l/controller/PersonController.java
+++ src/main/java/sg/edu/nus/iss/vttp5a_ssf_day13l/controller/PersonController.java
@@ -15,9 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 
 @Controller
@@ -63,5 +60,23 @@ public class PersonController {
         return "redirect:/persons";
     }
     
+    @GetMapping(path = "/update/{person-id}")
+    public String updateForm(@PathVariable("person-id") String personId, Model model) {
+        Person p = personService.findById(personId);
+        model.addAttribute("person", p);
+        return "personupdate";
+    }
+
+    @PostMapping("/update")
+    public String postUpdateForm(@Valid @ModelAttribute("person") Person person, BindingResult result, Model model) {
+
+        if (result.hasErrors())
+            return "personupdate";
+        
+        personService.update(person);
+
+        return "redirect:/persons";
+    }
+
     
 }
--- src/main/java/sg/edu/nus/iss/vttp5a_ssf_day13l/model/Person.java
+++ src/main/java/sg/edu/nus/iss/vttp5a_ssf_day13l/model/Person.java
@@ -10,7 +10,6 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
